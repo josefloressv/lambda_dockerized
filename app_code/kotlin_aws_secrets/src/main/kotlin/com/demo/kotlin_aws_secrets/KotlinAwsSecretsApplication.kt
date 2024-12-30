0964dbea98e2f069e59a00507ea3f7e0fb497f43
José Flores <josefloressv@gmail.com>
2024-12-29 19:43:58 -0600

    Add initial Kotlin AWS Secrets application setup

---
+++ app_code/kotlin_aws_secrets/src/main/kotlin/com/demo/kotlin_aws_secrets/KotlinAwsSecretsApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.kotlin_aws_secrets
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAwsSecretsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinAwsSecretsApplication>(*args)
+}
---
+++ app_code/kotlin_aws_secrets/src/test/kotlin/com/demo/kotlin_aws_secrets/KotlinAwsSecretsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.kotlin_aws_secrets
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAwsSecretsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
